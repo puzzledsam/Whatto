db144d5dcfd3510809106838ce2d9f08981fc27c
Samuel Dion <samuelqwe@icloud.com>
2022-01-18 19:01:46 -0500

    Refactor TMDB/JustWatch watch provider fetch and reimplement filtering

--- Whatto/Models/MainViewModel.swift
+++ Whatto/Models/MainViewModel.swift
@@ -14,46 +14,31 @@ class MainViewModel: ObservableObject {
         do {
             let SimklWatchlist = try await SimklAPI.shared.getWatchlist(accessToken: accessToken)
             
-            SimklWatchlist.movies?.forEach({ SimklMovie in
-                watchlist.addMovie(SimklMovie.movie, providers: []) // TODO: Finish re-implementing providers
-            })
+            for SimklMovie in SimklWatchlist.movies ?? [] {
+                let providers = await getMovieWatchProviders(tmdbId: SimklMovie.movie.ids?.tmdb ?? "")
+                watchlist.addMovie(SimklMovie.movie, providers: providers) // TODO: Finish re-implementing providers
+            }
         } catch {
             print("Watchlist fetch request failed with error: \(error)")
         }
     }
     
-    /*func getWatchlist(accessToken: String, completion: @escaping (Result<Bool,SimklAPI.APIError>) -> Void) {
-        SimklAPI.shared.getWatchlist(accessToken: accessToken) { [unowned self](result: Result<SimklWatchlist, SimklAPI.APIError>) in
-            switch result {
-            case .success(let decodedList):
-                
-                decodedList.movies?.forEach({ SimklMovie in
-                    watchlist.addMovie(SimklMovie.movie, providers: []) // TODO: Finish re-implementing providers
-                })
-                
-                completion(.success(true))
-            case .failure:
-                completion(.failure(SimklAPI.APIError.error))
-            }
-        }
-    }*/
-    
-    func getMovieWatchProviders(tmdbId: String, completion: @escaping (Result<[WatchProvider],TmdbAPI.APIError>) -> Void) {
+    func getMovieWatchProviders(tmdbId: String) async -> [WatchProvider] {
         var providerList: [WatchProvider] = []
         
-        TmdbAPI.shared.getMovieWatchProviders(movieId: Int(tmdbId) ?? 0) { (result: Result<TmdbMovieWatchProviders, TmdbAPI.APIError>) in
-            switch result {
-            case .success(let movieProviders):
-                for provider in (movieProviders.results?.ca?.flatrate ?? []) {
-                    if let validProvider = WatchProvider(rawValue: provider.providerName ?? "") {
-                        providerList.append(validProvider)
-                    }
+        do {
+            let movieProviders = try await TmdbAPI.shared.getMovieWatchProviders(movieId: Int(tmdbId) ?? 0)
+            
+            for provider in (movieProviders.results?.ca?.flatrate ?? []) {
+                if let validProvider = WatchProvider(rawValue: provider.providerName ?? "") {
+                    providerList.append(validProvider)
                 }
-            case .failure:
-                completion(.failure(TmdbAPI.APIError.error))
-                break
             }
+        } catch {
+            print("Provider fetch request failed for movie with ID \(tmdbId) with error: \(error)")
         }
+        
+        return providerList
     }
     
 }
